4320dbbce8979bedc8348f760080505241aa2cad
predat0r <alfa.clan@gmail.com>
2013-06-13 16:40:17 +0400

    работает уже лучше, но через жопу

--- 002-NasaRSS/src/person/tarasenko/nasarss/MainActivity.java
+++ 002-NasaRSS/src/person/tarasenko/nasarss/MainActivity.java
@@ -11,50 +11,54 @@ public class MainActivity extends Activity {
 
     public boolean refreshed;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	//	String url = (String)findViewById(R.string.link);
-		final IotdHandler handler = new IotdHandler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //	String url = (String)findViewById(R.string.link);
+        final IotdHandler handler = new IotdHandler();
         refreshed = false;
 
-        while (!refreshed){
-        new Thread(new Runnable() {
+        Thread prcessFeed = new Thread(new Runnable() {
             @Override
             public void run() {
                 refreshed = handler.processFeed();
 
             }
-        }).start();}
+        });
 
-        resetDisplay(handler.getTitle(), handler.getDate(), handler.getImage(), handler.getDescription());
+        prcessFeed.start();
+        while (!prcessFeed.isAlive()){
+            resetDisplay(handler.getTitle(), handler.getDate(), handler.getImage(), handler.getDescription());
+        }
 
-		System.out.println("All done");
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	private void resetDisplay(String title, String date, Bitmap image, StringBuffer description) {
-		TextView titleView = (TextView)findViewById(R.id.textTitle);
-		titleView.setText(title);
-		
-		TextView dateView = (TextView)findViewById(R.id.textDate);
-		dateView.setText(date);
-		
-		ImageView imageView = (ImageView)findViewById(R.id.imageD);		
-		imageView.setImageBitmap(image);
-		
-		TextView descriptionView = (TextView)findViewById(R.id.textDesc);
-		descriptionView.setText(description);
-		
-	}
-	
+
+        System.out.println("All done");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private void resetDisplay(String title, String date, Bitmap image, StringBuffer description) {
+        TextView titleView = (TextView) findViewById(R.id.textTitle);
+        titleView.setText(title);
+
+        TextView dateView = (TextView) findViewById(R.id.textDate);
+        dateView.setText(date);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageD);
+        imageView.setImageBitmap(image);
+
+        TextView descriptionView = (TextView) findViewById(R.id.textDesc);
+        descriptionView.setText(description);
+
+    }
+
 }
 
 
